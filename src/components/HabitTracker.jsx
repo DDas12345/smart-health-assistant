1a15f2de145fa7fed3c14f283cd3619383307b81
DDas12345 <das.debanshika@gmail.com>
2026-05-02 19:38:32 +0530

    Added functional habit tracker

--- src/components/HabitTracker.jsx
+++ src/components/HabitTracker.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import { useState } from 'react'
 
 function HabitTracker() {
+    const [habit, setHabit] = useState('')
+    const [habits, setHabits] = useState([])
+    function addHabit() {
+        if (habit.trim() === '') {
+            alert('Please enter a habit')
+            return
+        }
+        const newHabit = {
+            id: Date.now(),
+            name: habit,
+            completed: false
+        }
+        setHabits([...habits, newHabit])
+        setHabit('')
+    }
+
+    function toggleHabit(id) {
+        const updatedHabits = habits.map(h => {
+            if (h.id === id) {
+                return { ...h, completed: !h.completed }
+            }
+            return h
+        })
+        setHabits(updatedHabits)
+    }
     return (
-        <div>
+        <section className='section' id='habits'>
             <h2>Habit Tracker</h2>
-        </div>
+            <div className='tracker-box'>
+                <div className='input-row'>
+                    <input
+                        type='text'
+                        placeholder='Enter habit like walking, exercise...'
+                        value={habit}
+                        onChange={(e) => setHabit(e.target.value)}
+                    />
+                    <button onClick={addHabit}>Add Habit</button>
+                </div>
+                <div className='habit-list'>
+                    {habits.map((item) => (
+                        <div className="habit-item" key={item.id}>
+                            <span className={item.completed ? 'completed' : ''}>
+                                {item.name}
+                            </span>
+                            <div>
+                                <button onClick={() => toggleHabit(item.id)}>
+                                    {item.completed ? 'Undo' : 'Done'}
+                                </button>
+
+                                <button onClick={() => deleteHabit(item.id)}>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
 
